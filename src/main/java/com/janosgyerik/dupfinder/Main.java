01920020e03952b6bf3ab89c05b4dcdd5f75434b
Janos Gyerik <janos@kronos>
2016-01-16 19:08:07 +0100

    implement argument parsing using argparse4j

--- src/main/java/com/janosgyerik/dupfinder/Main.java
+++ src/main/java/com/janosgyerik/dupfinder/Main.java
@@ -4,6 +4,10 @@ import com.janosgyerik.dupfinder.dupfinder.DuplicateFileFinder;
 import com.janosgyerik.dupfinder.dupfinder.DuplicateFileFinderImpl;
 import com.janosgyerik.dupfinder.finder.FileFinder;
 import com.janosgyerik.dupfinder.finder.FileFinderImpl;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -13,15 +17,34 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            printUsage();
+        ArgumentParser parser = ArgumentParsers.newArgumentParser("java -jar dupfinder.jar")
+                .description("Find duplicate files in specified directory trees");
+        parser.addArgument("dirs")
+                .metavar("DIR")
+                .type(String.class)
+                .nargs("+")
+                .help("path to a directory");
+        parser.addArgument("--maxdepth")
+                .metavar("N")
+                .type(Integer.class)
+                .help("descend at most N directory levels");
+        parser.addArgument("--extension", "--ext")
+                .type(String.class)
+                .help("include only files matching extension");
+        try {
+            Namespace res = parser.parseArgs(args);
+            main(res);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
         }
+    }
 
+    private static void main(Namespace res) {
         FileFinder finder = new FileFinderImpl();
         DuplicateFileFinder duplicateFileFinder = new DuplicateFileFinderImpl();
 
-        for (String arg : args) {
-            File dir = new File(arg);
+        for (Object arg : res.getList("dirs")) {
+            File dir = new File(String.valueOf(arg));
             if (isAccessibleDirectory(dir)) {
                 List<File> files = finder.find(dir);
                 Set<Set<File>> duplicateFileSets = duplicateFileFinder.findDuplicates(files);
@@ -34,11 +57,6 @@ public class Main {
 
     private static boolean isAccessibleDirectory(File dir) {
         return dir.isDirectory() && dir.canRead() && dir.canExecute();
-    }
-
-    private static void printUsage() {
-        System.err.println("usage: java -jar dupfinder.jar DIRS...");
-        System.exit(1);
     }
 
     private static void printFileSet(Set<File> fileSet) {
