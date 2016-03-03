9cabb5247084e41750c7da54b0b7b2c8c0600f14
Janos Gyerik <janos@kronos>
2016-03-03 19:22:56 +0100

    rewrote filter by depth in terms of a file filter

--- src/main/java/com/janosgyerik/dupfinder/finder/FileFinderImpl.java
+++ src/main/java/com/janosgyerik/dupfinder/finder/FileFinderImpl.java
@@ -6,24 +6,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileFinderImpl extends SkeletalFileFinder {
+    private static class CompositeFileFilter implements FileFilter {
+        List<FileFilter> fileFilters = new ArrayList<>();
+
+        @Override
+        public boolean accept(File pathname) {
+            for (FileFilter fileFilter : fileFilters) {
+                if (!fileFilter.accept(pathname)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void add(FileFilter fileFilter) {
+            fileFilters.add(fileFilter);
+        }
+    }
+
+    private static class MaxDepthFileFilter implements FileFilter {
+        private final String prefix;
+        private final int maxDepth;
+
+        public MaxDepthFileFilter(File basedir, int maxDepth) {
+            this.prefix = basedir.toString();
+            this.maxDepth = maxDepth;
+        }
+
+        @Override
+        public boolean accept(File pathname) {
+            return pathname.toString().replace(prefix, "").split("/").length - 1 <= maxDepth;
+        }
+    }
+
     @Override
     public List<File> find(File basedir, FileFilter fileFilter, int depth) {
         List<File> files = new ArrayList<>();
-        find(basedir, fileFilter, depth, files);
+        CompositeFileFilter compositeFileFilter = new CompositeFileFilter();
+        compositeFileFilter.add(fileFilter);
+        compositeFileFilter.add(new MaxDepthFileFilter(basedir, depth));
+        find(basedir, compositeFileFilter, files);
         return files;
     }
 
-    public void find(File basedir, FileFilter fileFilter, int depth, List<File> acc) {
-        if (depth <= 0) {
-            return;
-        }
+    private void find(File basedir, FileFilter fileFilter, List<File> acc) {
         File[] files = basedir.listFiles(fileFilter);
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
                     acc.add(file);
                 } else if (file.isDirectory()) {
-                    find(file, fileFilter, depth - 1, acc);
+                    find(file, fileFilter, acc);
                 }
             }
         }
