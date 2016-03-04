d56a36b8f8de058b87a54dece761a43a95a1a520
Janos Gyerik <janos@kronos>
2016-03-04 21:56:37 +0100

    renamed pathname -> file

--- src/main/java/com/janosgyerik/dupfinder/FileFilters.java
+++ src/main/java/com/janosgyerik/dupfinder/FileFilters.java
@@ -5,26 +5,26 @@ import java.io.FileFilter;
 
 public class FileFilters {
     public static FileFilter any() {
-        return pathname -> true;
+        return file -> true;
     }
 
     public static FileFilter nonEmpty() {
-        return pathname -> pathname.length() > 0;
+        return file -> file.length() > 0;
     }
 
     public static FileFilter byExtension(String extension) {
-        return pathname -> pathname.isDirectory() || pathname.getName().endsWith(extension);
+        return file -> file.isDirectory() || file.getName().endsWith(extension);
     }
 
     public static FileFilter byMaxDepth(File basedir, int maxDepth) {
         String prefix = basedir.toString();
-        return pathname -> pathname.toString().replace(prefix, "").split("/").length - 1 <= maxDepth;
+        return file -> file.toString().replace(prefix, "").split("/").length - 1 <= maxDepth;
     }
 
     public static FileFilter composite(FileFilter... fileFilters) {
-        return pathname -> {
+        return file -> {
             for (FileFilter fileFilter : fileFilters) {
-                if (!fileFilter.accept(pathname)) {
+                if (!fileFilter.accept(file)) {
                     return false;
                 }
             }
